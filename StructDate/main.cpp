baae890d24fbf05be25da66087e28810ee6a2379
maho0math <160777388+maho0math@users.noreply.github.com>
2024-03-02 17:08:26 +0300

    lab_1_tmm.cpp

--- StructDate/main.cpp
+++ StructDate/main.cpp
@@ -4,53 +4,51 @@ using namespace std;
 
 void print_1(const int n)
 {
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
         cout << 1;
     cout << endl;
 }
 
 int main()
 {
-    cout << "Трифонов М.М" << endl;
-    cout << "090301-ПОВа-о23" << endl;
-    cout << "---------------" << endl;
+    setlocale(LC_ALL, "Russian");
+    cout << "Трифонов Матвей Михайлович \t" << "Группа: 090301 - ПОВа - о23\t" << "Курс: 1" << endl;
     int N;
-    cout << "input N =";
     cin >> N;
     if (N < 0)
     {
-        cout << "Error";
+        cout << "NO";
         return 0;
     }
-    if(N >= 1)
+    if (N >= 1)
     {
-        if( N >= pow(10,6))
+        if (N > pow(10, 6))
         {
-          cout << "Некорректные данные";  
+            cout << "NO";
         }
-        if(N <= pow(10,6))
+        if (N <= pow(10, 6))
         {
-            if(N % 2 == 0)
+            if (N % 2 == 0)
             {
-                cout << "Error";
+                cout << "NO";
                 return 0;
             }
-            if(N % 5 == 0)
+            if (N % 5 == 0)
             {
-                cout << "Error";
+                cout << "NO";
                 return 0;
             }
-            
-            if(N == 1)
+
+            if (N == 1)
             {
                 cout << 1;
                 return 0;
             }
-            
-             long int A = 1;
+
+            long int A = 1;
             for (long int i = 2; i <= N; i++)
             {
-                A = (A*10 + 1) % N;
+                A = (A * 10 + 1) % N;
                 if (A == 0)
                 {
                     print_1(i);
@@ -58,10 +56,10 @@ int main()
                 }
                 if (A == 1)
                 {
-                    print_1(i-1);
+                    print_1(i - 1);
                     return 0;
                 }
-                
+
             }
         }
     }
